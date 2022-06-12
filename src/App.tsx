7488a2dfaa0f424ab24af05afdbf845e650f7bc3
saidugurlu <ugurlumsaid@gmx.de>
2022-06-12 13:20:47 +0200

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-import './App.css';
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import Country from "./components/Country";
+import { CountryType } from "./types";
+import Loading from "./components/Loading";
 
 function App() {
+  const [countries, setCountries] = useState<CountryType[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const getCountries = async () => {
+    setLoading(true);
+    try {
+      const { data } = await axios.get<CountryType[]>(
+        "https://restcountries.eu/rest/v2/all"
+      );
+      setCountries(data);
+    } catch {
+      console.log("An error occurred during upload.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
   return (
-    <div className="App">
-   
+    <div>
+      <Loading loading={loading}>
+        {countries.map((country) => {
+          return <Country key={country.name} country={country} />;
+        })}
+      </Loading>
     </div>
   );
 }
